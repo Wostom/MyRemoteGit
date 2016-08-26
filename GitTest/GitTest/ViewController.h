6881f6165525613a46c210bcf37d4956cea5d6fe
mac <mac@zhang.local>
2016-08-26 15:51:50 +0800

    进行项目初始化

---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by mac on 16/8/26.
+//  Copyright © 2016年 zhizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by mac on 16/8/26.
+//  Copyright © 2016年 zhizhi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"xinlaide ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
